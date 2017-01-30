9d7bdd19dd2f4344e9f33ab86955102433adf99f
redchush <redchush@gmail.com>
2017-01-30 17:45:40 +0300

    rename module

--- core_/src/main/java/com/expert_soft/validator/group/G_Cart.java
+++
@@ -1,7 +0,0 @@
-package com.expert_soft.validator.group;
-
-
-public class G_Cart {
-
-    public interface Item{}
-}
--- core_/src/main/java/com/expert_soft/validator/group/G_Phone.java
+++
@@ -1,7 +0,0 @@
-package com.expert_soft.validator.group;
-
-
-public class G_Phone {
-
-    public interface Save{}
-}
